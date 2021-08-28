74a9695604ab163698ab4ca9967af2f6561a26b8
Nishikantaray <nishikantaray1@gmail.com>
2021-08-28 20:25:11 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { AdListComponent } from './ad-list/ad-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AdminComponent,
+    AdListComponent,
+   
   ],
   imports: [
     BrowserModule,

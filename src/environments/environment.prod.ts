4147a654d8ebbc85bea12160e29a08d8a58e2c3c
Nishikantaray <nishikantaray1@gmail.com>
2021-08-29 15:31:30 +0530

    added frontend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrlSubscriber: 'https://lootmagnet.herokuapp.com/api/subscriber/',
+  baseUrlAd: 'https://lootmagnet.herokuapp.com/api/advertiser/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   baseUrlSubscriber: 'https://lootmagnet.herokuapp.com/api/subscriber/',
   baseUrlAd: 'https://lootmagnet.herokuapp.com/api/advertiser/',
 };
